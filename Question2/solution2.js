0cf632332e60dfff85cf98cc2b83f1391da1da3c
yoshiodei <yoshiodei@yahoo.com>
2022-05-16 21:34:37 +0000

    solution2  complete

--- Question2/solution2.js
+++ Question2/solution2.js
@@ -1,5 +1,4 @@
-let numbers =  [2,46,378,362,83,1735,45,5,253,45]; //Random numbers
+let numbers =  [2,46,378,362,83,1735,45,5,253,45]; 
 let sortedNumber = numbers.sort((a,b)=> b-a);
 console.log(`Second largest number is ${sortedNumber[1]}`);
 
-// O(nlogn)
